9aca64fdb43acacc5788f15485ebd2e46a0185f1
Rayflex189 <roryisaiah66@gmail.com>
2025-02-05 05:12:37 -1100

    UPDATED

--- Axis/Axis/settings.py
+++ Axis/Axis/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Axis.urls'
@@ -119,8 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+if not DEBUG:
+    # Use WhiteNoise storage for production
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Additional locations of static files
