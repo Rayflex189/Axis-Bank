98ad4cd84896f0333f6382c6fb13b5bd45faeeda
Rayflex189 <roryisaiah66@gmail.com>
2025-02-10 05:48:56 -1100

    UPDATED

--- Axis/Axis/settings.py
+++ Axis/Axis/settings.py
@@ -11,6 +11,22 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'axis_bank_dbs',
+        'USER': 'axis_bank_dbs_user',
+        'PASSWORD': 'iDrB9NoD1wzys0SwdqW81gwLvxovzjpi',
+        'HOST': 'dpg-cul1vklumphs73bki24g-a.oregon-postgres.render.com',  # Check this
+        'PORT': '5432',
+    },
+     'OPTIONS': {
+            'sslmode': 'require',
+        },
+}
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,14 +92,6 @@ WSGI_APPLICATION = 'Axis.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
