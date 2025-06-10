e516bb83195634d9fc4375f70494a4e839c26a2f
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-06-09 21:23:28 -0700

    Create start.sh
    save

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Run migrations just in case (optional)
+python manage.py migrate
+
+# Run your custom management command
+python manage.py create_admin
+
+# Start Gunicorn server
+exec gunicorn Axis.wsgi:application --bind 0.0.0.0:2012
