acd657f91b0d704919c9adcb7ff29b335e2ee970
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-06-09 21:32:10 -0700

    Update settings.py
    Save

--- Axis/Axis/settings.py
+++ Axis/Axis/settings.py
@@ -49,7 +49,7 @@ SECRET_KEY = 'django-insecure-^)hb2(8rnlh=4s19#m0%@35)-87w#h5y(*s=(a1w%=xzh=@z%l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 
-ALLOWED_HOSTS = ["https://axisbank.fly.dev"]
+ALLOWED_HOSTS = ["axisbank.fly.dev"]
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dlzn0moho',
